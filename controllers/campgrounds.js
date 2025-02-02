f6e89cb8ed5e3086b1c44bc873fb63d5d7eefb22
DubeyArpita <arpitadubey23216@gmail.com>
2025-02-02 16:43:00 +0530

    Add files via upload

---
+++ controllers/campgrounds.js
@@ -0,0 +1,88 @@
+const Campground = require('../models/campground');
+const { cloudinary } = require("../cloudinary");
+const opencage = require('opencage-api-client');
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    if (req.headers.accept && req.headers.accept.includes('application/json')) {
+        return res.json(campgrounds); // Send JSON response if API request
+    }
+    res.render('campgrounds/index', { campgrounds })
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('campgrounds/new');
+}
+
+module.exports.createCampground = async (req, res, next) => {
+    const geoData = await opencage.geocode({
+        q: req.body.campground.location, // User's input location
+        key: '3636ee51fd8b4de98a5cadfe39053043' // Replace with your OpenCage API key
+    });
+
+    // If geocoding fails or returns no result
+    if (geoData.status.code !== 200 || geoData.results.length === 0) {
+        req.flash('error', 'Location not found. Please try again with a valid location!');
+        return res.redirect('back');
+    }
+
+    const campground = new Campground(req.body.campground);
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.author = req.user._id;
+    campground.geometry = {
+        type: "Point", // GeoJSON type
+        coordinates: [geoData.results[0].geometry.lng, geoData.results[0].geometry.lat] // Longitude and latitude
+    };
+    await campground.save();
+    console.log(campground);
+    req.flash('success', 'Successfully made a new campground!');
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.showCampground = async (req, res,) => {
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground });
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit', { campground });
+}
+
+module.exports.updateCampground = async (req, res) => {
+    const { id } = req.params;
+    console.log(req.body);
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
+    req.flash('success', 'Successfully updated campground!');
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.deleteCampground = async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground')
+    res.redirect('/campgrounds');
+}
